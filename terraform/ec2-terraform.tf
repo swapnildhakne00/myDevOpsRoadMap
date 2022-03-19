ba2c0f1df62e3ac802c734efc2a1195d6edcd7be
root <root@Swapnil-PC.localdomain>
2022-03-19 19:22:26 +0530

    all setup now

---
+++ terraform/ec2-terraform.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+resource "aws_instance" "myweb" {
+  ami           = "ami-04893cdb768d0f9ee"
+  instance_type = "t2.micro"
+}
