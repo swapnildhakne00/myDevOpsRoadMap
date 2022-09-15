b38b3dd4d7963ae37efe8fe758f7f7cd17189811
swapnildhakne28194 <113577185+swapnildhakne28194@users.noreply.github.com>
2022-09-15 11:49:34 +0530

    Update ec2-terraform.tf

--- terraform/ec2-terraform.tf
+++ terraform/ec2-terraform.tf
@@ -10,7 +10,7 @@ terraform {
 provider "aws" {
   region = "ap-south-1"
 }
-resource "aws_instance" "myweb1" {
+resource "aws_instance" "myinstance" {
   ami           = "ami-04893cdb768d0f9ee"
   instance_type = "t2.micro"
 }
